74314fb3b9dec17651637c065b91aefdd875ac69
huuudrich <108597096+huuudrich@users.noreply.github.com>
2023-07-08 19:21:20 +1100

    Update TrackServiceImpl.java

--- music-content-service/src/main/java/com/example/services/TrackServiceImpl.java
+++ music-content-service/src/main/java/com/example/services/TrackServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.services;
 
+import com.example.models.users.User;
 import com.example.repositorys.AlbumRepository;
 import com.example.repositorys.TracksRepository;
+import com.example.repositorys.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.EntityNotFoundException;
 
 
 @Service
@@ -14,8 +18,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class TrackServiceImpl implements TracksService {
     private final AlbumRepository albumRepository;
     private final TracksRepository tracksRepository;
+    private final UserRepository userRepository;
 
-    public void uploadTracks(MultipartFile[] files) {
+    public void uploadTracks(Long userId, MultipartFile[] files) {
+    }
 
+    private User findUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("User with id: %d not found", userId)));
     }
 }
